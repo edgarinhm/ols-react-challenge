9654dda0739a520dd7a45d5671cb3873e36cd8ca
Edgar Mejia <edgarinhm@hotmail.com>
2024-06-13 12:21:33 -0500

    OL-01 - eslint prettier setup

--- ols-challenge-react-typescript-vite/.eslintrc.cjs
+++ ols-challenge-react-typescript-vite/.eslintrc.cjs
@@ -3,16 +3,21 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/newline-after-import': ['error', { count: 1 }],
   },
 }
