9be292eb77ecbb00a44048c85241f818b54ad1ca
Edgar Mejia <edgarinhm@hotmail.com>
2024-06-14 15:35:51 -0500

    OL-03 - add mixins panels, popovers, tooltips, scrollbars OL-03 - add checbox form control OL-03 - refactor login form inputs styles

--- src/common/components/form-control/form-control.tsx
+++ src/common/components/form-control/form-control.tsx
@@ -17,9 +17,19 @@ interface InputControlProps
   onClear?: () => void;
 }
 
+interface CheckInputControlProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    BaseControlProps {
+  type: 'radio' | 'checkbox';
+  labelClassName?: string;
+}
+
 type FormControlType = {
   Input: React.ForwardRefExoticComponent<
     React.RefAttributes<HTMLInputElement> & InputControlProps
+  >;
+  CheckInput: React.ForwardRefExoticComponent<
+    React.RefAttributes<HTMLInputElement> & CheckInputControlProps
   >;
 };
 
@@ -63,8 +73,37 @@ const Input = forwardRef<HTMLInputElement, InputControlProps>((props, ref) => {
   );
 });
 
+const CheckInput = forwardRef<HTMLInputElement, CheckInputControlProps>(
+  (props, ref) => {
+    const { id, label, showErrors, errors, type, labelClassName, ...rest } =
+      props;
+    const displayError = showErrors && !!errors?.length;
+
+    return (
+      <div
+        className={
+          displayError
+            ? `${style.formControl} ${style.error}`
+            : style.formControl
+        }
+      >
+        <div className={style.checkInput}>
+          <input id={id} ref={ref} type={type} {...rest} />
+          {!!label && (
+            <label htmlFor={id} className={labelClassName}>
+              {label}
+            </label>
+          )}
+        </div>
+        {displayError && <p>{errors[0]}</p>}
+      </div>
+    );
+  }
+);
+
 const FormControl: FormControlType = {
   Input: Input,
+  CheckInput: CheckInput,
 };
 
 export { FormControl };
--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,33 +1,61 @@
-import logo from '/logo.png';
+import logoImg from '/logo.png';
 import styles from './login.module.scss';
-import { FormEvent } from 'react';
+import { FormEvent, useId, useState } from 'react';
 import { FormControl } from 'common/components/form-control/form-control';
+import { Link } from 'react-router-dom';
 
 const Login = (): JSX.Element => {
+  const id = useId();
+  const [expireSession, setExpireSession] = useState(false);
   const handleOnSubmit = (event: FormEvent): void => {
     event.preventDefault();
-    console.log('handleOnSubmit');
+    console.log('handleOnSubmit', expireSession);
+  };
+  const onCheckboxChange = (): void => {
+    setExpireSession((value) => !value);
   };
   return (
     <div className={styles.container}>
-      <img src={logo} alt={'logo'} loading="lazy" className={styles.logo} />
-      <span>{'Bienvenido al gestor de proyectos!'}</span>
-      <span>{'Necesitamos tu usuario y contraseña'}</span>
       <div className={styles.login}>
-        <form noValidate autoComplete="off" onSubmit={handleOnSubmit}>
-          <FormControl.Input
-            type="text"
-            placeholder={'Nombre de usuario Ej: nombre.apellido'}
-            className={styles.input}
-          />
-          <FormControl.Input
-            type="password"
-            placeholder={'Aqui va tu constraseña'}
-          />
-          <button type="submit" className={styles.loginButton}>
-            {'Ingresar'}
-          </button>
-        </form>
+        <div className={styles.loginPanel}>
+          <div className={styles.loginPanelBody}>
+            <header>
+              <Link to="#">
+                <img src={logoImg} loading="lazy" alt={'login logo'} />
+              </Link>
+              <h1>{'Bienvenido al gestor de proyectos!'}</h1>
+              <span>{'Necesitamos tu usuario y contraseña'}</span>
+            </header>
+
+            <form noValidate autoComplete="off" onSubmit={handleOnSubmit}>
+              <FormControl.Input
+                id={`${id}-username`}
+                type="text"
+                placeholder={'Nombre de usuario Ej: nombre.apellido'}
+                className={styles.input}
+              />
+              <FormControl.Input
+                id={`${id}-password`}
+                type="password"
+                placeholder={'Aqui va tu constraseña'}
+              />
+              <button type="submit" className={styles.loginButton}>
+                {'Ingresar'}
+              </button>
+              <div className={styles.actions}>
+                <FormControl.CheckInput
+                  type="checkbox"
+                  checked={expireSession}
+                  id={`${id}-expire-session`}
+                  label={'Permanecer Conectado'}
+                  labelClassName={styles.standardLabel}
+                  onChange={onCheckboxChange}
+                />
+                <Link to="#">{'Recuperar Contraseña'}</Link>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
