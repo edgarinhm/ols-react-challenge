e4ea4b0344c48625d27328b98a2e7a104b5855af
Edgar Mejia <edgarinhm@hotmail.com>
2024-06-17 09:30:12 -0500

    OL-21 fix docker port OL-21 add host vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import styles from './app.module.scss';
-import { AppRouter } from './Router';
-import TopBar from 'components/top-bar/top-bar';
+import { Route, Routes } from "react-router-dom";
+import styles from "./app.module.scss";
+import { AppRouter } from "./router";
+import TopBar from "components/top-bar/top-bar";
 
 const App = (): JSX.Element => {
   return (
     <Routes>
       <Route
-        path={'*'}
+        path={"*"}
         element={
           <div className={styles.app}>
             <TopBar />
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,32 @@
 /// <reference types="vitest" />
 // https://vitejs.dev/config/
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'happy-dom',
-    setupFiles: './src/setupTests.ts',
+    environment: "happy-dom",
+    setupFiles: "./src/setupTests.ts",
   },
   server: {
+    host: true,
     port: 3000,
     proxy: {
-      '^/v\\d+/.*': {
-        target: 'http://localhost:8080',
+      "^/v\\d+/.*": {
+        target: "http://localhost:8080",
         changeOrigin: true,
       },
     },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      common: `${path.resolve(__dirname, './src/common/')}`,
-      components: `${path.resolve(__dirname, './src/components/')}`,
-      routes: `${path.resolve(__dirname, './src/routes')}`,
-      router: `${path.resolve(__dirname, './src/Router')}`,
+      common: `${path.resolve(__dirname, "./src/common/")}`,
+      components: `${path.resolve(__dirname, "./src/components/")}`,
+      routes: `${path.resolve(__dirname, "./src/routes")}`,
+      router: `${path.resolve(__dirname, "./src/router")}`,
     },
   },
 });
