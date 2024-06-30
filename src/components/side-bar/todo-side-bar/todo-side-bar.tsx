6578c1f16f55f5c62abed650fd9e6a68e974369f
Edgar Mejia <edgarinhm@hotmail.com>
2024-06-30 18:06:41 -0500

    OL-42 add todo sidebar layout transition

--- src/components/side-bar/todo-side-bar/todo-side-bar.tsx
+++ src/components/side-bar/todo-side-bar/todo-side-bar.tsx
@@ -1,24 +1,17 @@
 import { useSharedStorage } from "common/state-management/shared-storage";
 import styles from "./todo-side-bar.module.scss";
-import { Collapse } from "common/components/collapse/collapse";
 
 const TodoSideBar = () => {
   const isTodoSideBarOpen = useSharedStorage((state) => state.isTodoSideBarOpen);
 
   return (
-    <Collapse
-      collapse={Boolean(isTodoSideBarOpen)}
-      timeout={1000}
-      delay={350}
-      /*enterEasing="linear"
-      exitEasing="ease"*/
-      /*enterEasing="ease-in-out"
-      exitEasing="ease"
-      direction={"vertical"}*/
-      exitEasing="ease"
-    >
-      <div className={styles.todoSideBar}>TodoSideBar</div>
-    </Collapse>
+    <div className={`${styles.todoSideBar} ${Boolean(isTodoSideBarOpen) ? styles.open : ""}`}>
+      <div className={styles.title}>{"Pendientes"}</div>
+      <div className={styles.action}>
+        {"Que tienes pendiente?"} <button>{"Agregar"}</button>
+      </div>
+      <div className={styles.body}>body</div>
+    </div>
   );
 };
 
