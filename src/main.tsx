9995036aeff62504e7d291e8e5eea58b4477a778
Edgar Mejia <edgarinhm@hotmail.com>
2024-07-07 02:19:14 -0500

    OL-57 fix bearer logger messages rule

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         resetErrorBoundary(error);
         return <></>;
       }}
-      onReset={(error) => console.error(`Critical Application Error: ${error}`)}
+      onReset={(error) => console.error(`Critical Application Error: ${error.reason}}`)}
     >
       <BrowserRouter>
         <AuthenticationProvider>
