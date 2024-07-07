e042bedf4e8825e007bf98f4fbf2bc8d926bae48
Edgar Mejia <edgarinhm@hotmail.com>
2024-07-06 21:00:39 -0500

    OL-53 add homepage github page OL-53 add vite base path github page OL-53 add devsecops pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ols-react-challenge/", //github pages
   test: {
     globals: true,
     environment: "happy-dom",
