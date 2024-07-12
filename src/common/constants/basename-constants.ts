47ca59f9a67a46a94f9facfdf765a816563821a8
Edgar Mejia <edgarinhm@hotmail.com>
2024-07-11 21:45:20 -0500

    OL-66 fix basename logic

--- src/common/constants/basename-constants.ts
+++ src/common/constants/basename-constants.ts
@@ -1,2 +1,2 @@
 const { ReactRouteId } = window["environment-config" as keyof typeof window] ?? {};
-export const basename = `/${ReactRouteId ?? import.meta?.env?.BASE_URL}`;
+export const basename = `${ReactRouteId ? "/" + ReactRouteId + "/" : import.meta.env?.BASE_URL ?? "/"}`;
