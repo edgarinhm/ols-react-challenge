5b5174806aa2548ab3969ffe543a68cd3c731920
edgarinhm <edgarinhm@users.noreply.github.com>
2024-07-13 16:39:28 +0000

    deploy: 0f7bd89edd178ce4afc95e101b31f5e83734e0b6

--- environment-config.js
+++ environment-config.js
@@ -1,16 +1,6 @@
-// NOTE: variable keys and values need to be in double quotes to prevent
-// the deployment script from accidentally replacing the wrong values
-
-// prettier-ignore
-window["environment-config"] = Object.freeze({
-    "ReactRouteId": "ols-react-challenge",
-    "Environment": "local",
-    "OlWebsiteDomain": "",
-    "APIHostName": "http://localhost:3000",
-  });
-  {
+window[environment-config] = Object.freeze({
     ReactRouteId: ols-react-challenge,
     Environment: staging,
     OlWebsiteDomain: edgarinhm.github.io,
     APIHostName: http://localhost:3000,
-  }
+  });
