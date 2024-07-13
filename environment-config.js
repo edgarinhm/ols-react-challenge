207bd770962de9474448f5699a9d7fcd6010f507
edgarinhm <edgarinhm@users.noreply.github.com>
2024-07-13 17:06:45 +0000

    deploy: cc6db62df87709a46640563da514f6455c42f48f

--- environment-config.js
+++ environment-config.js
@@ -1,6 +1,6 @@
 window["environment-config"] = Object.freeze({
     "ReactRouteId": "ols-react-challenge",
-    "Environment": "staging",
+    "Environment": "stage",
     "OlWebsiteDomain": "https://edgarinhm.github.io",
     "APIHostName": "http://localhost:3000",
   });
