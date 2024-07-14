8593d66a3a54a9443ebf6f691898de0481b20eb6
Edgar Mejia <edgarinhm@hotmail.com>
2024-07-14 13:45:58 -0500

    OL-dashboard-server-report add commit report to dashboard

--- src/components/dashboard/commits-report.tsx
+++ src/components/dashboard/commits-report.tsx
@@ -1,14 +1,16 @@
-import styles from "./server-report.module.scss";
-const CommitsReport = () => {
-  <div className={styles.commitsReport}>
-    <p>{"Reporte De Commits"}</p>
-    <p>
-      {
-        "Total de commits realizados por cada mes diferenciando entre los tag de Ajustes(fix) y Caracteristicas(feat)"
-      }
-    </p>
-    <div className={styles.chartContainer}></div>
-  </div>;
+import styles from "./commits-report.module.scss";
+const CommitsReport = (): JSX.Element => {
+  return (
+    <div className={styles.commitsReport}>
+      <p>{"Reporte De Commits"}</p>
+      <p>
+        {
+          "Total de commits realizados por cada mes diferenciando entre los tag de Ajustes(fix) y Caracteristicas(feat)"
+        }
+      </p>
+      <div className={styles.chartContainer}></div>
+    </div>
+  );
 };
 
 export default CommitsReport;
--- src/components/dashboard/reports.tsx
+++ src/components/dashboard/reports.tsx
@@ -1,9 +1,11 @@
+import CommitsReport from "./commits-report";
 import ServerReport from "./server-report";
 
 const Reports = (): JSX.Element => {
   return (
     <div>
       <ServerReport />
+      <CommitsReport />
     </div>
   );
 };
