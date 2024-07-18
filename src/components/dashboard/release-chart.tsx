8689356fde155bbffcb28c2e04311a1a246df346
Edgar Mejia <edgarinhm@hotmail.com>
2024-07-18 14:22:19 -0500

    OL-dashboard-detailed-release-chart fix linter warning

--- src/components/dashboard/release-chart.tsx
+++ src/components/dashboard/release-chart.tsx
@@ -1,4 +1,4 @@
-import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer, LegendProps } from "recharts";
 import styles from "./release-chart.module.scss";
 
 interface ReleaseChartProps {
@@ -6,28 +6,6 @@ interface ReleaseChartProps {
 }
 
 const ReleaseChart = ({ data }: ReleaseChartProps) => {
-  const RenderColorfulLegendText = (props) => {
-    const { payload } = props;
-    return (
-      <>
-        {payload.map((entry, index) => (
-          <div key={entry.value} className={styles.legend}>
-            <div className={styles.iconGroup}>
-              <div
-                className={styles.icon}
-                style={{
-                  backgroundColor: entry.color,
-                }}
-              ></div>
-              <p className={styles.label}>{entry.value}</p>
-            </div>
-            <p className={styles.value}>{entry.payload.value}</p>
-          </div>
-        ))}
-      </>
-    );
-  };
-
   return (
     <ResponsiveContainer width="100%" height="100%" aspect={500 / 300}>
       <PieChart width={600} height={300}>
@@ -38,11 +16,33 @@ const ReleaseChart = ({ data }: ReleaseChartProps) => {
           layout="vertical"
           verticalAlign="bottom"
           iconSize={10}
-          content={<RenderColorfulLegendText />}
+          content={<CustomLegendText />}
         />
         <Pie cx="50%" cy="50%" data={data} dataKey="value" outerRadius={100} innerRadius={80} />
       </PieChart>
     </ResponsiveContainer>
+  );
+};
+
+const CustomLegendText = ({ payload }: LegendProps) => {
+  return (
+    <>
+      {payload &&
+        payload.map((entry) => (
+          <div key={entry.value} className={styles.legend}>
+            <div className={styles.iconGroup}>
+              <div
+                className={styles.icon}
+                style={{
+                  backgroundColor: entry.color,
+                }}
+              ></div>
+              <p className={styles.label}>{entry.value}</p>
+            </div>
+            <p className={styles.value}>{entry.payload?.value}</p>
+          </div>
+        ))}
+    </>
   );
 };
 
